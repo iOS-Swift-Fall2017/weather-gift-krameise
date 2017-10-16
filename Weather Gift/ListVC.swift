e0001f066d4e20e54eb4a85d1647476069da359e
Michelle Krameisen <michellekrameisen@michelles-macbook-pro-2.local>
2017-10-16 16:10:19 -0400

    7.7

--- Weather Gift/ListVC.swift
+++ Weather Gift/ListVC.swift
@@ -12,6 +12,10 @@ class ListVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
     var locationsArray = [String]()
     var currentPage = 0
     
@@ -29,6 +33,18 @@ class ListVC: UIViewController {
         }
     }
     
+    @IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing == true {
+        tableView.setEditing(false, animated: true)
+        editBarButton.title = "Edit"
+        addBarButton.isEnabled = true
+        } else {
+            tableView.setEditing(true, animated: true)
+            editBarButton.title = "Done"
+            addBarButton.isEnabled = false
+        }
+    }
+
 }
 
 extension ListVC: UITableViewDelegate, UITableViewDataSource {
@@ -40,5 +56,46 @@ extension ListVC: UITableViewDelegate, UITableViewDataSource {
         cell.textLabel?.text = locationsArray[indexPath.row]
         return cell
         }
+    //MARK: - Table View Editing Functions
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            locationsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = locationsArray[sourceIndexPath.row]
+        locationsArray.remove(at: sourceIndexPath.row)
+        locationsArray.insert(itemToMove, at: destinationIndexPath.row)
+    }
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+//        if indexPath.row != 0 {
+//            return true
+//        } else {
+//            return false
+//        }
+         return (indexPath.row != 0 ? true: false)
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+//        if indexPath.row != 0 {
+//            return true
+//        } else {
+//            return false
+//        }
+        return (indexPath.row != 0 ? true: false)
+    }
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+//        if proposedDestinationIndexPath.row == 0 {
+//            return sourceIndexPath
+//        } else {
+//            return proposedDestinationIndexPath
+//        }
+//
+        return (proposedDestinationIndexPath.row == 0 ? sourceIndexPath : proposedDestinationIndexPath)
+    }
+
 }
 
